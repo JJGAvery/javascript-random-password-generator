ff8c5c064fa218895438288e6cb504ba510facee
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-01-14 16:52:37 -0700

    added calling function for the password defining prompts on generate password button click, also updated readme file for initial description and technologies that will be used.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # javascript-random-password-generator
+
+## **Link to deployed application**
+
+
+## **Description**
+In this challenge I was presented with starter code in html, css and javascript. There was no modifications needed to the html or css code.  
+
+## **Technologies**
+- HTML
+- CSS
+- JAVASCRIPT
+
+## **Preview**
+![Javascript Password Generator]()   
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -13,3 +13,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// calling function for prompts on generate password on button click
+function generatePassword(){
+
+  passwordLength = prompt("How many characters do you want your password to be? \nPassword must be between 8 and 128 characters.");
+
+  window.confirm("Do you want to use any lower case letters?");
+  window.confirm("Do you want to use any upper case letters?");
+  window.confirm("Do you want to use any special characters?");
+  window.confirm("Do you want to use any numbers?");
+
+}

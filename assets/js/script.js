29c8736226b0be7334b27357fbbebf933138b42b
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-01-14 20:15:51 -0700

    added all of the password item arrays of lowercase letters, uppercase letters, special characters and numbers.

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,24 @@ function generatePassword(){
   window.confirm("Do you want to use any numbers?");
 
 }
+
+//Create password items
+const passwordCriteria = {
+
+  //Lowercase letters array
+  passwordLowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+  //Uppercase letters array
+  passwordUpperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+
+  //Special characters array
+  passwordSpecialCharacter: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+",
+  "=", "<", ">", "?"],
+
+  //Numbers array
+  passwordNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+}
+
